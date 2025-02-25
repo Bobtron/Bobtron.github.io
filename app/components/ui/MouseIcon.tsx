5fdb88fa27f2a5b2c5492644bfe31336cbc7a61c
Justin L <justinian.the.iii@gmail.com>
2025-02-25 00:23:30 -0800

    Moved css to tailwindcss global file

--- app/components/ui/MouseIcon.tsx
+++ app/components/ui/MouseIcon.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type React from "react"
-import styles from "./MouseIcon.module.css"
 
 interface MouseIconProps {
     onClick?: () => void
@@ -9,11 +8,11 @@ interface MouseIconProps {
 
 const MouseIcon: React.FC<MouseIconProps> = ({ onClick }) => {
     return (
-        <span className={styles.scrollLink} onClick={onClick}>
+    <span className="scrollLink" onClick={onClick}>
       <svg className="max-w-10 w-full h-auto" viewBox="0 0 76 130">
         <g fill="none">
           <rect className="stroke-black stroke-3" width="70" height="118" x="1.5" y="1.5" rx="36" />
-          <circle className={styles.scroll} cx="36.5" cy="31.5" r="4.5" />
+          <circle className="scroll" cx="36.5" cy="31.5" r="4.5" />
         </g>
       </svg>
     </span>
